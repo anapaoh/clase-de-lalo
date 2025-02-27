20e89ac50e5bc5de1ddd3268eb769af742d9499f
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-02-27 12:49:38 -0600

    Agregar ejs

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(bodyParser.urlencoded({extended: false})); //esta línea cada vez que se manda algo de post, llega en bytes y lo tranforma en texto; lo transforma en un bojeto con el que se puede interactuar
-
-//Middleware. Todo lo que se hace en express se usan middlewares.
+//Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
-
     //Le permite a la petición avanzar hacia el siguiente middleware
     next(); 
 });
-
 const plantasRoutes = require('./routes/plantas.routes');
-
-app.use('/plantas', plantasRoutes); //aquí lo que se hace es que se cuelguen todas las rutas plantas que aparezca ese archivo. Si el localhost noincluye nada que diga plantas aparece lo de hola mundo
-
+app.use('/plantas', plantasRoutes);
 app.use((request, response, next) => {
     console.log('Otro middleware!');
     
     //Manda la respuesta
-    response.send('¡Hola mundo!'); /*status(404).send("No se encuentra el recurso que estás buscando)*/
+    response.statusCode = 404;
+    response.send('No se encuentra el recurso que estás buscando'); 
 });
-
 app.listen(3000);
