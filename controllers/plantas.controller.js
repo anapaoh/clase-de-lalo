04e739be5e21ef8c052e5475a8d732b99c65132e
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-03-03 13:00:00 -0600

    cambios de clase de lalo

--- controllers/plantas.controller.js
+++ controllers/plantas.controller.js
@@ -1,28 +1,25 @@
-exports.get_agregar =  (request, response, next) => {
-    response.send(html_header + html_form + html_footer);
+const Planta = require('../models/planta.models');
+
+exports.get_agregar = (request, response, next) => {
     response.render('agregar_planta');
 };
 
-
 exports.post_agregar = (request, response, next) => {
     console.log(request.body);
-    plantas.push(request.body.nombre);
-    let html = html_header;
-    html += `<div class="columns">`;
-    for(let planta of plantas) {
-        html += `<div class="column">`;
-        html += `<div class="card">
-        <div class="card-content">
-          <div class="content">`;
-        html += planta;
-        html += `</div>
-                </div>
-              </div>
-            </div>`;
-    }
-    html += `</div>`;
-    html += html_footer;
-    response.send(html);
+    const mi_planta = new Planta(requiest.body.nombre);
+    mi_planta.save();
+
+    response.redirect('/plantas/')
+
+    //response.render('lista_plantas', {
+    //    plantas: Planta.fetchAll,
+    //});
+}
+
+exports.get_root = (request, response, next) => {
+    response.render('lista_plantas', {
+        plantas: Planta.fetchAll(),
+    });
 };
 
 exports.get_regar =  (request, response, next) => {
--- routes/plantas.routes.js
+++ routes/plantas.routes.js
@@ -3,13 +3,17 @@ const router = express.Router();
 
 const plantas_controller = require('../controllers/plantas.controller');
 
-const plantas = [];
-
 //router.get es para registrar un middleware para peticiones HTTP GET
 router.get('/agregar', plantas_controller.get_agregar);
+router.get('/add', plantas_controller.get_agregar);
 
 //router.post es para registrar un middleware para peticiones HTTP POST
 router.post('/agregar', plantas_controller.post_agregar);
 
 const path = require('path');
+
 router.get('/regar', plantas_controller.get_regar);
+
+router.get('/', plantas_controller.get_root);
+
+module.exports = router;
