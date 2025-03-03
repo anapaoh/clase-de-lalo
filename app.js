49202ef6751b1f69b0bd78346ab0700c824115e7
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-03-02 19:48:18 -0600

    Al fin lab 5

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
+
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -16,6 +15,7 @@ app.use((request, response, next) => {
     //Le permite a la petición avanzar hacia el siguiente middleware
     next(); 
 });
+
 const plantasRoutes = require('./routes/plantas.routes');
 app.use('/plantas', plantasRoutes);
 app.use((request, response, next) => {
@@ -25,4 +25,3 @@ app.use((request, response, next) => {
     response.statusCode = 404;
     response.send('No se encuentra el recurso que estás buscando'); 
 });
-app.listen(3000);
